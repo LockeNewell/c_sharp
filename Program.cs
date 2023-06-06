b85431ebaf63b3dd244cc203a95100f0f10e757b
Locke Newell <locke.newell@gmail.com>
2023-06-06 14:13:35 -0500

    add questions and solutions
    this push only includes the solutions but should be fixed to allow the console app user to pick the question. and run it more then once.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,157 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the array of integers (comma-separated):");
+        string input = Console.ReadLine();
+
+        int[] nums = input.Split(',').Select(int.Parse).ToArray();
+
+        var mostFrequent = nums
+            .GroupBy(num => num)
+            .OrderByDescending(group => group.Count())
+            .FirstOrDefault();
+
+        if (mostFrequent != null)
+        {
+            Console.WriteLine("The most frequently occurring element is " + mostFrequent.Key);
+        }
+        else
+        {
+            Console.WriteLine("No elements found in the array.");
+        }
+    }
+}
+
+
+/* using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the array of integers (comma-separated):");
+        string input = Console.ReadLine();
+
+        string[] numStrings = input.Split(',');
+        int[] nums = new int[numStrings.Length];
+
+        for (int i = 0; i < numStrings.Length; i++)
+        {
+            nums[i] = int.Parse(numStrings[i]);
+        }
+
+        int num1 = 0;
+        int num2 = 0;
+        int minSum = int.MaxValue;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int sum = nums[i] + nums[j];
+                int absoluteSum = Math.Abs(sum);
+
+                if (absoluteSum < minSum)
+                {
+                    minSum = absoluteSum;
+                    num1 = nums[i];
+                    num2 = nums[j];
+                }
+            }
+        }
+
+        Console.WriteLine("The two numbers whose sum is closest to zero are " + num1 + " and " + num2);
+    }
+}
+
+ */
+
+/* using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a sentence:");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+        Array.Reverse(words);
+
+        string reversedSentence = string.Join(" ", words);
+
+        Console.WriteLine("Reversed sentence: " + reversedSentence);
+    }
+} */
+
+
+/* using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the array of integers (comma-separated):");
+        string input = Console.ReadLine();
+
+        string[] numStrings = input.Split(',');
+        int[] nums = new int[numStrings.Length];
+
+        for (int i = 0; i < numStrings.Length; i++)
+        {
+            nums[i] = int.Parse(numStrings[i]);
+        }
+
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        int sum = 0;
+
+        foreach (int num in nums)
+        {
+            if (num < min)
+                min = num;
+            if (num > max)
+                max = num;
+            sum += num;
+        }
+
+        int expectedSum = (max - min + 1) * (min + max) / 2;
+        int missingNumber = expectedSum - sum;
+
+        Console.WriteLine("The missing number is: " + missingNumber);
+    }
+} */
+
+/* using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (charCounts.ContainsKey(c))
+                charCounts[c]++;
+            else
+                charCounts[c] = 1;
+        }
+
+        var sortedCharCounts = charCounts.OrderBy(x => x.Key);
+
+        foreach (var kvp in sortedCharCounts)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+} */
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # c_sharp
+
 c# interview questions and their answers
+
+## Write a C# program that accepts a string as input and counts the number of occurrences of each character in the string. The program should then display the character and its corresponding count in alphabetical order.
+
+Example:
+Input: "hello"
+Output:
+e: 1
+h: 1
+l: 2
+o: 1
+
+## Write a C# program to find the missing number in an array of consecutive integers.
+
+Example:
+Input: [1, 2, 4, 5, 6]
+Output: The missing number is 3
+
+## Write a C# program that reverses the order of words in a given sentence.
+
+Example:
+Input: "Hello, how are you?"
+Output: "you? are how Hello,"
+
+## Challenge: Write a C# program that finds the two numbers in an array whose sum is closest to zero.
+
+Example:
+Input: 1, -3, 2, 5, -4, 7, -2
+Output: The two numbers whose sum is closest to zero are -2 and 2
+
+## using LINQ write a C# program that finds the most frequently occurring element in an array.
+
+Example:
+Input: 2, 3, 2, 5, 6, 3, 2, 3, 3, 5
+Output: The most frequently occurring element is 3
