6314ea70d32f3ab641725a0dca3fa85904636a2c
Locke Newell <locke.newell@gmail.com>
2023-06-08 09:37:08 -0500

    add main loop and threading mongodb question
    this adds a main loop that will alow the user to try each of the questions. and adds a question answer about threading mongodb

--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 class Program
 {
     static void Main()
+    {
+        string input = "1";
+        while ( !input.Equals("Q")){
+          Console.WriteLine("Enter the array of integers (comma-separated):");
+          input = Console.ReadLine();
+          switch(input) {
+            case "1":
+              charCount();
+              break;
+            case "2":
+              missingNumber();
+              break;
+            case "3":
+              reverseOrder();
+              break;
+            case "4":
+              closestToZero();
+              break;
+            case "5":
+              mostFrequentlyOccurring();
+              break;
+            case "6":
+              threading();
+              break;
+            default:
+              // code block
+              break;
+          }
+          
+        }
+    }
+    static void charCount()
     {
         Console.WriteLine("Enter the array of integers (comma-separated):");
         string input = Console.ReadLine();
@@ -24,14 +60,8 @@ class Program
             Console.WriteLine("No elements found in the array.");
         }
     }
-}
 
-
-/* using System;
-
-class Program
-{
-    static void Main()
+    static void missingNumber()
     {
         Console.WriteLine("Enter the array of integers (comma-separated):");
         string input = Console.ReadLine();
@@ -66,15 +96,8 @@ class Program
 
         Console.WriteLine("The two numbers whose sum is closest to zero are " + num1 + " and " + num2);
     }
-}
 
- */
-
-/* using System;
-
-class Program
-{
-    static void Main()
+    static void reverseOrder()
     {
         Console.WriteLine("Enter a sentence:");
         string input = Console.ReadLine();
@@ -86,14 +109,8 @@ class Program
 
         Console.WriteLine("Reversed sentence: " + reversedSentence);
     }
-} */
 
-
-/* using System;
-
-class Program
-{
-    static void Main()
+    static void closestToZero()
     {
         Console.WriteLine("Enter the array of integers (comma-separated):");
         string input = Console.ReadLine();
@@ -124,15 +141,7 @@ class Program
 
         Console.WriteLine("The missing number is: " + missingNumber);
     }
-} */
-
-/* using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Program
-{
-    static void Main()
+    static void mostFrequentlyOccurring()
     {
         Console.Write("Enter a string: ");
         string input = Console.ReadLine();
@@ -154,4 +163,44 @@ class Program
             Console.WriteLine($"{kvp.Key}: {kvp.Value}");
         }
     }
-} */
+
+    static void threading()
+    {
+        // MongoDB connection string and database name
+        string connectionString = "mongodb://localhost:27017";
+        string databaseName = "your_database_name";
+
+        // Create a MongoDB client
+        var client = new MongoClient(connectionString);
+
+        // Access the database
+        var database = client.GetDatabase(databaseName);
+
+        // Access the collection
+        var collection = database.GetCollection<BsonDocument>("your_collection_name");
+
+        // Create a thread to fetch the documents
+        Thread thread = new Thread(() =>
+        {
+            // Retrieve documents from the collection
+            var documents = collection.Find(new BsonDocument()).ToList();
+
+            // Print the documents to the screen
+            foreach (var document in documents)
+            {
+                Console.WriteLine(document);
+            }
+        });
+
+        // Start the thread
+        thread.Start();
+
+        // Wait for the thread to complete
+        thread.Join();
+
+        // Prompt user to exit the program
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # c_sharp
 
-c# interview questions and their answers
+## setup
 
-## Write a C# program that accepts a string as input and counts the number of occurrences of each character in the string. The program should then display the character and its corresponding count in alphabetical order.
+this program requires MongoDB drivers to run
+'Install-Package MongoDB.Driver'
+
+## c# interview questions and their answers
+
+### Write a C# program that accepts a string as input and counts the number of occurrences of each character in the string. The program should then display the character and its corresponding count in alphabetical order.
 
 Example:
 Input: "hello"
@@ -12,13 +17,13 @@ h: 1
 l: 2
 o: 1
 
-## Write a C# program to find the missing number in an array of consecutive integers.
+### Write a C# program to find the missing number in an array of consecutive integers.
 
 Example:
 Input: [1, 2, 4, 5, 6]
 Output: The missing number is 3
 
-## Write a C# program that reverses the order of words in a given sentence.
+### Write a C# program that reverses the order of words in a given sentence.
 
 Example:
 Input: "Hello, how are you?"
@@ -30,8 +35,10 @@ Example:
 Input: 1, -3, 2, 5, -4, 7, -2
 Output: The two numbers whose sum is closest to zero are -2 and 2
 
-## using LINQ write a C# program that finds the most frequently occurring element in an array.
+### using LINQ write a C# program that finds the most frequently occurring element in an array.
 
 Example:
 Input: 2, 3, 2, 5, 6, 3, 2, 3, 3, 5
 Output: The most frequently occurring element is 3
+
+### write sample code where a c# program contacts a mongodb database uses threading and prints the result to the screen
